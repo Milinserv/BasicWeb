5a34b1891ebd8047f20e5c93bba1023b86b06ad1
Milinserv <107671454+Milinserv@users.noreply.github.com>
2024-01-18 20:25:12 +0400

    added configuration files

---
+++ boot.php
@@ -0,0 +1,29 @@
+<?php
+// Инициализируем сессию
+session_start();
+
+// Простой способ сделать глобально доступным подключение в БД
+function pdo(): PDO
+{
+    static $pdo;
+
+    if (!$pdo) {
+        if (file_exists(__DIR__ . '/config.php')) {
+            $config = include __DIR__.'/config.php';
+        } else {
+            $msg = 'Создайте и настройте config.php на основе config.sample.php';
+            trigger_error($msg, E_USER_ERROR);
+        }
+        // Подключение к БД
+        $dsn = 'mysql:dbname='.$config['db_name'].';host='.$config['db_host'];
+        $pdo = new PDO($dsn, $config['db_user'], $config['db_pass']);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    return $pdo;
+}
+//Проверяем авторизован ли пользователь
+function check_auth(): bool
+{
+    return !!($_SESSION['user_id'] ?? false);
+}
---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'db_name' => 'one',
+    'db_host' => 'localhost',
+    'db_user' => 'root',
+    'db_pass' => 'root',
+];
